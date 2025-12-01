410190f8432df0bfbb327d6c52af8e9fedce4664
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 15:52:21 +0100

    [1 28] update ex4.py

--- day1/ex4.py
+++ day1/ex4.py
@@ -5,4 +5,17 @@ ws = wb.active
 
 ws = wb['vgsales']
 
+row_position = 2
+col_position = 7
+
+total_sales = (
+        (ws.cell(row=row_position, column=col_position).value)
+        + (ws.cell(row=row_position, column=col_position + 1).value)
+        + (ws.cell(row=row_position, column=col_position + 2).value)
+        + (ws.cell(row=row_position, column=col_position + 3).value)
+)
+
+ws.cell(row=2, column=11).value = total_sales
+
+wb.save("video2.xlsx")
 wb.close()
