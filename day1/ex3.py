20b3bc42cc6e8922bde9c10b22e3885cdb6808b5
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 15:23:49 +0100

    [1 22] update ex3.py

--- day1/ex3.py
+++ day1/ex3.py
@@ -1,4 +1,19 @@
 import openpyxl
 
 wb = openpyxl.load_workbook("../data/videogamesales.xlsx")
+# wb = openpyxl.load_workbook("videogamesales.xlsx")
 ws = wb.active
+
+print(wb)  # <openpyxl.workbook.workbook.Workbook object at 0x000001A2248656A0>
+print(ws)  # <Worksheet "vgsales">
+
+ws = wb['vgsales']  # ustawienie arkusza do działania
+
+print("Total number of rows:", ws.max_row)  # Total number of rows: 16328
+print("Total number of columns:", ws.max_column)  # Total number of columns: 10
+
+print("Values in cell A1 is:", ws['A1'].value)
+
+value = [ws.cell(row=i, column=i).value for i in range(1, ws.max_column + 1)]
+
+wb.close()
