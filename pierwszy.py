54dd658302f651c1956ae5ab2c28eca1a3d59764
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 11:36:20 +0100

    [1 3] update pierwszy

--- pierwszy.py
+++ pierwszy.py
@@ -27,3 +27,26 @@ print(type(45))  # <class 'int'>, liczby całkowite
 print(sys.int_info)
 # sys.int_info(bits_per_digit=30, sizeof_digit=4, default_max_str_digits=4300,
 # str_digits_check_threshold=640)
+
+print("34" + "19")  # 3419, konkatenacja, łączenie stringów
+
+print(34 + 19)  # 53
+
+# print("35" + 29)  # TypeError: can only concatenate str (not "int") to str
+
+print(34 * "168")
+print(34 * 168)
+
+print(10 * "-")  # ----------
+
+print(4.56)  # liczby zmiennoprzecinkowe
+print(type(4.56))  # <class 'float'>
+print(sys.float_info)
+# sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308,
+# min=2.2250738585072014e-308, min_exp=-1021
+# , min_10_exp=-307, dig=15, mant_dig=53, epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+# błąd zaokrąglenia
+print(0.1 + 0.2)  # 0.30000000000000004
+print(0.1 + 0.9)  # 1.0
+# decimal - pozwala ominąc problem zokrąglenia
