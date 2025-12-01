f839686ef94f64db864655225d266cd4b85f4b85
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 15:29:13 +0100

    [1 23] update ex3.py

--- day1/ex3.py
+++ day1/ex3.py
@@ -1,6 +1,6 @@
 import openpyxl
 
-wb = openpyxl.load_workbook("../data/videogamesales.xlsx")
+wb = openpyxl.load_workbook(r"../data/videogamesales.xlsx")
 # wb = openpyxl.load_workbook("videogamesales.xlsx")
 ws = wb.active
 
@@ -14,6 +14,9 @@ print("Total number of columns:", ws.max_column)  # Total number of columns: 10
 
 print("Values in cell A1 is:", ws['A1'].value)
 
-value = [ws.cell(row=i, column=i).value for i in range(1, ws.max_column + 1)]
+value = [ws.cell(row=1, column=i).value for i in range(1, ws.max_column + 1)]
+print(value)
+# ['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
+
 
 wb.close()
