32739622877eb253854c822d9ad67f10c1557096
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 15:40:27 +0100

    [1 25] update ex3.py

--- day1/ex3.py
+++ day1/ex3.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import openpyxl
 
 wb = openpyxl.load_workbook(r"../data/videogamesales.xlsx")
@@ -19,7 +21,7 @@ print(value)
 # ['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
 
 # dane z kolumny 2
-data = [ws.cell(row=i, column=2).value for i in range(2, 12)] # od 2 do 11
+data = [ws.cell(row=i, column=2).value for i in range(2, 12)]  # od 2 do 11
 print(data)
 
 # ['Wii Sports',
@@ -34,5 +36,33 @@ print(data)
 # 'New Super Mario Bros. Wii',
 # 'Duck Hunt']
 
+my_list = list()  # pusta lista
+for value in ws.iter_rows(
+        min_row=1,
+        max_row=11,
+        min_col=1,
+        max_col=6,
+        values_only=True):
+    my_list.append(value)
 
+# print(my_list)
+pprint(my_list)
 wb.close()
+# [('Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher'),
+#  (1, 'Wii Sports', 'Wii', 2006, 'Sports', 'Nintendo'),
+#  (2, 'Super Mario Bros.', 'NES', 1985, 'Platform', 'Nintendo'),
+#  (3, 'Mario Kart Wii', 'Wii', 2008, 'Racing', 'Nintendo'),
+#  (4, 'Wii Sports Resort', 'Wii', 2009, 'Sports', 'Nintendo'),
+#  (5, 'Pokemon Red/Pokemon Blue', 'GB', 1996, 'Role-Playing', 'Nintendo'),
+#  (6, 'Tetris', 'GB', 1989, 'Puzzle', 'Nintendo'),
+#  (7, 'New Super Mario Bros.', 'DS', 2006, 'Platform', 'Nintendo'),
+#  (8, 'Wii Play', 'Wii', 2006, 'Misc', 'Nintendo'),
+#  (9, 'New Super Mario Bros. Wii', 'Wii', 2009, 'Platform', 'Nintendo'),
+#  (10, 'Duck Hunt', 'NES', 1984, 'Shooter', 'Nintendo')]
+
+
+for ele1, ele2, ele3, ele4, ele5, ele6 in my_list:
+    print(f"{ele1:<8}{ele2:>35}{ele3:^10}{ele4:<10}{ele5:<15}{ele6:<10}")
+# < - wyrównie do lewej
+# > do prawej
+# ^ wyśrodkowane
