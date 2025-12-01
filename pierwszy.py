c029d8a0aa7b4d161a1e0b5aaffd22e0ce25b876
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 11:00:44 +0100

    [1 1] init

---
+++ pierwszy.py
@@ -0,0 +1,19 @@
+# https://peps.python.org/pep-0008/
+# snake_case
+
+# ctrl alt l - formatowanie
+print("Radek")  # wydrukuj/wypisz, Radek
+print("")
+print("")
+print('Radek')  # Radek
+
+# ctrl / - komentarz
+# print("Radek')
+#   File "C:\Users\CSComarch\PycharmProjects\PythonMSExcel_1_12_2025\pierwszy.py", line 10
+#     print("Radek')
+#           ^
+# SyntaxError: unterminated string literal (detected at line 10)
+#
+# Process finished with exit code 1
+
+
