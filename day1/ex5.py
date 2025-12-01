a015b763e6437aef107b114ad52d1650fa29563b
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-01 15:58:45 +0100

    [1 31] update ex5.py

--- day1/ex5.py
+++ day1/ex5.py
@@ -7,5 +7,19 @@ ws = wb.active
 
 ws = wb['vgsales']
 
+row_position = 1
+
+for i in range(1, ws.max_row):
+    row_position += 1
+    print(row_position)
+    NA_Sales = ws.cell(row=row_position, column=7).value
+    EU_Sales = ws.cell(row=row_position, column=8).value
+    JP_Sales = ws.cell(row=row_position, column=9).value
+    Other_Sales = ws.cell(row=row_position, column=10).value
+
+    total_sales = (NA_Sales + EU_Sales + JP_Sales + Other_Sales)
+
+    ws.cell(row=row_position, column=11).value = total_sales
+
 wb.save(filename)
 wb.close()
