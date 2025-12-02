6da12a135c75e489f285d71590cc148d41c6a2bc
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 12:22:54 +0100

    [2 30] update ex14

--- day2/ex15.py
+++ day2/ex15.py
@@ -1,8 +1,19 @@
 import pandas as pd
 
+#  pip install xlsxwriter
 data = [
     ["Adiya", 179],
     ["Samen", 181],
     ["Darek", 170],
-    ["JAn", 167],
+    ["Jan", 167],
 ]
+
+column_names = ['Name', "Height"]
+
+df = pd.DataFrame(data, columns=column_names)
+
+writer = pd.ExcelWriter("excel_with_list.xlsx", engine="xlsxwriter")
+
+# df.to_excel(writer)
+df.to_excel(writer, index=False)  # bez kolumny index
+writer.close()
