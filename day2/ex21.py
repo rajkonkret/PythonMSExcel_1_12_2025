96e74206179987b53b3041413de3e13ba053949d
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 14:28:42 +0100

    [2 48] update ex21

--- day2/ex21.py
+++ day2/ex21.py
@@ -5,8 +5,33 @@
 # dopisac te wyniki
 
 # dane
+import pandas as pd
+
 data = {
     "Produkt": ["Laptop", "Myszka", "Laptop", "Monitor", "Myszka", "Klawiatura"],
-    "Ilość":   [2,        5,        1,        1,         3,        2],
-    "Cena_jedn": [3500,   80,       3600,     900,       75,       120],
+    "Ilość": [2, 5, 1, 1, 3, 2],
+    "Cena_jedn": [3500, 80, 3600, 900, 75, 120],
 }
+
+df = pd.DataFrame(data)
+wejscie = "sprzedaz_dane.xlsx"
+df.to_excel(wejscie, index=False)
+
+df = pd.read_excel(wejscie)
+
+df['Wartość'] = df['Ilość'] * df['Cena_jedn']
+
+# zwróci dataframe
+podsumowanie = (
+    df.groupby("Produkt")[["Ilość", "Wartość"]]
+    .sum()
+    .reset_index()
+    .sort_values(by="Wartość", ascending=False)
+)
+
+wyjscie = "raport_sprzedaz.xlsx"
+with pd.ExcelWriter(wyjscie, engine="openpyxl") as writer:
+    df.to_excel(writer, sheet_name="Dane", index=False)
+    podsumowanie.to_excel(writer, sheet_name="Podsumowanie", index=False)
+
+print('plik został zapiany:', wyjscie)
