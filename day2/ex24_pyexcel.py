341b6b57827c20d53596917ad2d8a2f37e54488b
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 14:41:56 +0100

    [2 52] update ex24

--- day2/ex24_pyexcel.py
+++ day2/ex24_pyexcel.py
@@ -1,2 +1,12 @@
 import pyexcel
+
 #  pip install pyexcel pyexcel-xlsx
+
+data = [
+    ["Imię", "Wiek"],
+    ["Tomek", 38],
+    ['Kasia', 37],
+]
+
+sheet = pyexcel.Sheet(data)
+sheet.save_as('wyniki.xlsx')
