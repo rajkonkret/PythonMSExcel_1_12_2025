9e69ea1447ba43e6b0e65b7358e90d3498d2fce5
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 11:35:07 +0100

    [2 19] update ex12.py

--- day2/ex12.py
+++ day2/ex12.py
@@ -10,8 +10,29 @@ ws = wb.active
 print(ws.title)
 
 # rgb -> FF0000 -> red
+# https://www.rapidtables.com/web/color/RGB_Color.html
 ws['A1'].font = Font(color="FF0000", bold=True, size=12)
 ws['A2'].font = Font(color="0000FF")
 
+ws['A1'].fill = PatternFill('lightVertical', start_color="38e3ff")
+# FILL_NONE = 'none'
+# FILL_SOLID = 'solid'
+# FILL_PATTERN_DARKDOWN = 'darkDown'
+# FILL_PATTERN_DARKGRAY = 'darkGray'
+# FILL_PATTERN_DARKGRID = 'darkGrid'
+# FILL_PATTERN_DARKHORIZONTAL = 'darkHorizontal'
+# FILL_PATTERN_DARKTRELLIS = 'darkTrellis'
+# FILL_PATTERN_DARKUP = 'darkUp'
+# FILL_PATTERN_DARKVERTICAL = 'darkVertical'
+# FILL_PATTERN_GRAY0625 = 'gray0625'
+# FILL_PATTERN_GRAY125 = 'gray125'
+# FILL_PATTERN_LIGHTDOWN = 'lightDown'
+# FILL_PATTERN_LIGHTGRAY = 'lightGray'
+# FILL_PATTERN_LIGHTGRID = 'lightGrid'
+# FILL_PATTERN_LIGHTHORIZONTAL = 'lightHorizontal'
+# FILL_PATTERN_LIGHTTRELLIS = 'lightTrellis'
+# FILL_PATTERN_LIGHTUP = 'lightUp'
+# FILL_PATTERN_LIGHTVERTICAL = 'lightVertical'
+# FILL_PATTERN_MEDIUMGRAY = 'mediumGray'
 wb.save('video3.xlsx')
 wb.close()
