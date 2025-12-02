545f35c3c363e5102203c510ff9fdd4c0626d355
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 13:56:44 +0100

    [2 46] update ex20

--- day2/ex20.py
+++ day2/ex20.py
@@ -17,3 +17,40 @@ print(df[df['Height'] > 175])
 
 suma = df['Height'].sum(skipna=False)  # skipna - omin brakujące, NaN
 print(suma)  # 697
+
+df.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 4 entries, 0 to 3
+# Data columns (total 2 columns):
+#  #   Column  Non-Null Count  Dtype
+# ---  ------  --------------  -----
+#  0   Name    4 non-null      object
+#  1   Height  4 non-null      int64
+# dtypes: int64(1), object(1)
+# memory usage: 196.0+ bytes
+
+print(df.describe())
+#            Height
+# count    4.000000
+# mean   174.250000
+# std      6.800735
+# min    167.000000
+# 25%    169.250000
+# 50%    174.500000
+# 75%    179.500000
+# max    181.000000
+
+print(df['Height'].mean())  # 174.25
+print(df['Height'].median())  # 174.5
+
+# dla konkretnej kolumny
+print(df['Height'].describe())
+# count      4.000000
+# mean     174.250000
+# std        6.800735
+# min      167.000000
+# 25%      169.250000
+# 50%      174.500000
+# 75%      179.500000
+# max      181.000000
+# Name: Height, dtype: float64
