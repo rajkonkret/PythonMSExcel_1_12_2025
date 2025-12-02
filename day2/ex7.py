f1dc2f724ae5c963885e7f6b36cf1a616004c352
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 09:39:09 +0100

    [2 5] update ex7.py

--- day2/ex7.py
+++ day2/ex7.py
@@ -4,3 +4,13 @@ filename = 'video2.xlsx'
 
 wb = openpyxl.load_workbook(filename)
 ws = wb['vgsales']
+
+# ws['P1'] = 'Average Sales'
+# ws['P2'] = '=AVERAGE(K2:K16328)'
+#
+
+# ws['Q1'] = "Number of populated cells"
+# ws['Q2'] = "=COUNTA(E2:E16328)"
+
+wb.save(filename)
+wb.close()
