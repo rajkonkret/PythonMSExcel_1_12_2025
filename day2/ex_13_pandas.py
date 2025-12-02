9e8fa8d552278e25d1a98571583c291a480cef99
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 12:01:51 +0100

    [2 24] update ex13_pandas

---
+++ day2/ex_13_pandas.py
@@ -0,0 +1,7 @@
+# pandas - biblioteka do danych tabelarycznych
+# DataFrame - odpowiednik tablicy/ macierzy
+import pandas as pd
+# pip install pandas
+
+writer = pd.ExcelWriter('empty_excel.xlsx')
+empty_dataframe = pd.DataFrame() # tablica/ macierz
