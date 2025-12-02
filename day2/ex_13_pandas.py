cbcb2f14e98a71bea709f3105aeb34dd15f217a5
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 12:03:18 +0100

    [2 25] update ex13_pandas

--- day2/ex_13_pandas.py
+++ day2/ex_13_pandas.py
@@ -1,7 +1,11 @@
 # pandas - biblioteka do danych tabelarycznych
 # DataFrame - odpowiednik tablicy/ macierzy
 import pandas as pd
+
 # pip install pandas
 
 writer = pd.ExcelWriter('empty_excel.xlsx')
-empty_dataframe = pd.DataFrame() # tablica/ macierz
+empty_dataframe = pd.DataFrame()  # tablica/ macierz
+
+empty_dataframe.to_excelwriter(sheet_name='empty')
+writer.close()  # przy pracy z writerem musimy używać close()
