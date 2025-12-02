1e29c3fb446c3810b933a0443709653f52297fe7
rajkonkret660 <rajkonkret660@gmial.com>
2025-12-02 12:27:58 +0100

    [2 31] update ex14

--- day2/ex15.py
+++ day2/ex15.py
@@ -12,8 +12,11 @@ column_names = ['Name', "Height"]
 
 df = pd.DataFrame(data, columns=column_names)
 
+
 writer = pd.ExcelWriter("excel_with_list.xlsx", engine="xlsxwriter")
+# sprawdzenie wyłaczenia pogrubienia kolumn
 
 # df.to_excel(writer)
-df.to_excel(writer, index=False)  # bez kolumny index
+# df.to_excel(writer, index=False)  # bez kolumny index
+df.to_excel(writer, index=False, sheet_name='first_sheet')  # nazwa arkusza
 writer.close()
